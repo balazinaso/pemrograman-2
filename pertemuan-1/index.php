d60663355538a5ff03145875eb29a53788e6f301
balazinaso <147615633+balazinaso@users.noreply.github.com>
2024-04-23 19:00:47 +0700

    Create index.php

---
+++ pertemuan-1/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas Pertemuan Pertama</title>
+    <style>
+        h1{
+            color: red;
+            text-align: center;
+        }
+        table{
+            width: 300px;
+            margin: 0 auto;
+            color: blue;
+            font-size: 1.5em;
+        }
+        .nama{
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+   <?php echo "
+   <h1>BIODATA</h1> 
+   <table cellpadding='5' cellspacing='0' border='0'>
+    <tr>
+        <td>Nama</td>
+        <td>:</td>
+        <td class='nama'>Balazi</td>
+    </tr>
+    <tr>
+        <td>Email</td>
+        <td>:</td>
+        <td>balazi@gmail.com</td>
+    </tr>
+    <tr>
+        <td>Jurusan</td>
+        <td>:</td>
+        <td>Teknik Informatika</td>
+    </tr>
+    <tr>
+        <td>Hoby</td>
+        <td>:</td>
+        <td>Tidur</td>
+    </tr>
+   </table>
+   " ?>
+</body>
+</html>
